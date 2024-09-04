95b6da287022b4a82c6590c31cab980dad1f738e
saraisa12 <saraisa.024@gmail.com>
2024-09-04 21:56:37 +0300

    read me and pseudo code

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# whack-a-mole
+# whack A Mole
+
+## Date: 12/09/2024
+
+### By: Sara Alhusaini
+
+[GitHub](https://github.com/saraisa12)
+|
+[Linkden](http://www.duckduckgo.com)
+
+---
+
+### **_Description_**
+
+##### whak a mole is a web based game where players try to whack moles appearing randomly, the goal is to hit as many as possible within the time frame.
+
+---
+
+#### **_Technologies Used_**
+
+- Front end
+
+  - html
+  - css
+
+- Logic
+  - javascript
+
+---
+
+#### **_Getting Started_**
+
+##### steps in how to get started.
+
+---
+
+#### **_Screenshots_**
+
+### chilling
+
+- will be posted later
+
+---
+
+#### **_Future Work_**
+
+- will be posted later
+
+---
+
+#### **_Credits_**
+
+- will be posted later
---
+++ script.js
@@ -0,0 +1,21 @@
+// Initialize global variables
+// Define variables for score, timer, mole position, mole visibility, game state, etc.
+
+// Set up a random mechanism to display the mole
+// Use random method to make the mole appear
+
+// Make the mole go up and down
+// Display the mole for a brief time, then hide it
+
+// The mole should only be clickable if it is visible
+// Ignore clicks when the mole is not shown
+
+// The user should be able to start the game
+// Implement a "Start" button to trigger the game
+
+// A timer should be displayed, and the game should stop when the counter ends
+
+// The mole should be displayed for a few seconds
+
+// Score should increase when the mole is clicked
+// Increment the score and update the display whenever the mole is clicked
